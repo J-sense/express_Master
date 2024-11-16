b0abcd0f5143717cc3b4a671fa9d7e3b8410e3ff
J-sense <jishan1873@gmail.com>
2024-11-16 21:28:43 +0600

    routing

--- dist/app/app.js
+++ dist/app/app.js
@@ -6,7 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express_1.default.json());
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get('/', logger, (req, res) => {
+    res.send('Hello warrrrr!');
+});
+app.post('/', logger, (req, res) => {
+    console.log(req.body);
+    res.send("kjfkldjsf");
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,18 @@
-import express from 'express'
+import express, { NextFunction,Request, Response} from 'express'
 const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const port = 3000;
+app.use(express.json())
+const logger = (req:Request,res:Response, next:NextFunction)=>{
+    console.log(req.url,req.method,req.hostname)
+    next();
+}
+app.get('/',logger,(req:Request, res:Response) => {
+  res.send('Hello warrrrr!');
+ 
+})
+app.post('/',logger,(req,res)=>{
+    console.log(req.body)
+    res.send("kjfkldjsf")
 })
 
 
